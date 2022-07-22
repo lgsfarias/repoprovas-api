beb9e6df174c09bb8446923e4afe259334ddb77c
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-07-22 11:53:44 -0300

    fix: include confirm password in signup schema

--- src/schemas/signup.schema.ts
+++ src/schemas/signup.schema.ts
@@ -1,9 +1,17 @@
 import Joi from 'joi';
 import { CreateUserData } from '../repositories/user.repository';
 
-const signupSchema = Joi.object<CreateUserData>({
+type CreateUserDataWithConfirmPassword = CreateUserData & {
+  confirmPassword: string;
+};
+
+const signupSchema = Joi.object<CreateUserDataWithConfirmPassword>({
   email: Joi.string().email().required(),
   password: Joi.string().min(10).required(),
+  confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+    'any.only': 'Passwords must match',
+    'any.required': 'Confirm password is required',
+  }),
 }).required();
 
 export default signupSchema;
