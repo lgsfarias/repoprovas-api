fcf79668a3407aaace0e4b0ed1140c97d6b359a8
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-07-22 12:24:52 -0300

    fix: type of schemas

--- src/schemas/signin.schema.ts
+++ src/schemas/signin.schema.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi';
+import { CreateUserData } from '../repositories/user.repository.js';
 
-const signinSchema = Joi.object({
+const signinSchema = Joi.object<CreateUserData>({
   email: Joi.string().email().required(),
   password: Joi.string().min(10).required(),
 }).required();
--- src/schemas/test.schema.ts
+++ src/schemas/test.schema.ts
@@ -1,11 +1,17 @@
 import Joi from 'joi';
-import { CreateTestData } from '../repositories/test.repository';
+import { CreateTestData } from '../repositories/test.repository.js';
 
-const createTestSchema = Joi.object<CreateTestData>({
+type CreateTestSchema = Omit<CreateTestData, 'teacherDisciplineId'> & {
+  teacherId: number;
+  disciplineId: number;
+};
+
+const createTestSchema = Joi.object<CreateTestSchema>({
   name: Joi.string().required(),
   pdfUrl: Joi.string().uri().required(),
   categoryId: Joi.number().integer().positive().required(),
-  teacherDisciplineId: Joi.number().integer().positive().required(),
+  teacherId: Joi.number().integer().positive().required(),
+  disciplineId: Joi.number().integer().positive().required(),
 }).required();
 
 export default createTestSchema;
